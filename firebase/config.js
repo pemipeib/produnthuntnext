174b3d27bba847298ea41c37252269ef5d0b911f
Periko <pemipeib@msn.com>
2020-04-04 18:20:21 +0200

    350 Creando el context...

---
+++ firebase/config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB2j7f-wTcv15JJX_QlP4dZjRZp1BKH-_0",
+    authDomain: "product-hunt-de40c.firebaseapp.com",
+    databaseURL: "https://product-hunt-de40c.firebaseio.com",
+    projectId: "product-hunt-de40c",
+    storageBucket: "product-hunt-de40c.appspot.com",
+    messagingSenderId: "1048112670711",
+    appId: "1:1048112670711:web:55c07b8763ef27bfd3b0f2"
+};
+
+export default firebaseConfig;
